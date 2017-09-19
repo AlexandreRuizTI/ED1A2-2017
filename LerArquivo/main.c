fe929923d9086476c77cb0b08e7dbd3c6f87b3af
AlexandreRuizTI <31457019+AlexandreRuizTI@users.noreply.github.com>
2017-09-19 19:53:48 -0300

    LerArquivo

---
+++ LerArquivo/main.c
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   main.c
+ * Author: Alexandre
+ *
+ * Created on 19 de Setembro de 2017, 19:19
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ *
+ */
+int main(int argc, char** argv) {
+    char nomeArq[] = "nomes.text";
+    char ch1[10][10];
+    FILE *arq;
+    int n = 0;
+    arq = fopen(nomeArq, "r");
+    if (arq == NULL) {
+        printf("Erro,não foi possivel abrir o arquivo\n");
+    } else {
+        while ((fscanf(arq, "%s\n", &ch1[n])) != EOF && n < 10) {
+            printf("%s\n", ch1[n]);
+            n++;
+        }
+    }
+    fclose(arq);
+
+    return (EXIT_SUCCESS);
+}
+
